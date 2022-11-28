7d7a3eac5a6f04ad4d159e42b2a0557d733bbba4
taras <tarik2002sta@gmail/com>
2022-11-28 12:15:40 +0200

    firct commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const firstNumber = Number(prompt('Будь ласка вкажіть перше число ♡'));
+const secondNumber = Number(prompt('Будь ласка вкажіть друге число ♡'));
+
+
+
+const addResult = Number(firstNumber + secondNumber);
+const subResult = Number(firstNumber - secondNumber);
+const multResult = Number(firstNumber * secondNumber);
+const divResult = Number(firstNumber / secondNumber);
+
+alert(firstNumber + '+' + secondNumber + '='+ addResult);
+alert(firstNumber + '-' + secondNumber + '='+ subResult);
+alert(firstNumber + '*' + secondNumber + '='+ multResult);
+alert(firstNumber + '/' + secondNumber + '='+ divResult);
